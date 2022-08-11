0e623ec6b0e541db32b6131bd1c0939a841608e7
Tamara Kathleen <tkathleen@ciandt.com>
2022-08-11 10:07:17 -0300

    App that has a purchase screen and cart, this app makes the exchange of screens through Tab Navigation.

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import TelaPadrao from './src/componentes/TelaPadrao';
+import Rotas from './src/Rotas';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  console.log("Alura");
+  return <TelaPadrao><Rotas /></TelaPadrao>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
